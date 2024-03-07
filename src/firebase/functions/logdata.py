cbbc322c24248805a043e26bbfd39bc7ca63fb3c
Karl McCoubrey <karlmccoubrey@gmail.com>
2024-03-07 20:55:44 +0000

    Removal of match statement due to python3.9 versioning on RPi

--- src/firebase/functions/logdata.py
+++ src/firebase/functions/logdata.py
@@ -4,13 +4,25 @@ def log_data_to_firebase(item):
     matchItem = item['action']
     
     # match statement to determine action to handle 
-    match matchItem:
-        case 'getTemp':
-            # log temp
-            log_temperature_to_firebase(item['data'])
-        case _:
-            # default
-            print('default case hit for logDataToFirebase. Please check data and try again.')
+    #match matchItem:
+    #    case 'getTemp':
+    #        # log temp
+    #        log_temperature_to_firebase(item['data'])
+    #    case _:
+    #        # default
+    #        print('default case hit for logDataToFirebase. Please check data and try again.')
+    
+    
+    if matchItem == 'getTemp':
+        # log temperature
+        log_temperature_to_firebase(item['data'])
+    elif matchItem == 'PLACEHOLDER': 
+        # DO SOMETHING
+        print('placeholder elif')
+    else:
+        #default
+        print('default case hit for logDataToFirebase. Please check data and try again.')
+    
             
             
             
