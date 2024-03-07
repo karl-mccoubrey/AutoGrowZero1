0b0a83de72491b30a5d3a480840cd9358fe897d8
Karl McCoubrey <karlmccoubrey@gmail.com>
2024-03-07 01:52:29 +0000

    Dict update for Item, utilised witin firebase functions as API.

--- src/firebase/functions/logdata.py
+++ src/firebase/functions/logdata.py
@@ -1,9 +1,9 @@
 
 def log_data_to_firebase(item):
-    match item.action:
+    match item['action']:
         case 'getTemp':
             # log temp
-            log_temperature_to_firebase(item.data)
+            log_temperature_to_firebase(item['data'])
         case _:
             # default
             print('default case hit for logDataToFirebase. Please check data and try again.')
